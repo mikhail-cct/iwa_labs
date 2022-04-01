194cc9d277f035432f00afbe40658258f68ef1ce
mikhail-cct <tayemnyj+cct@gmail.com>
2022-04-01 08:29:13 +0000

    fix server in index.js

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ const dbURI = "mongodb://localhost/test";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((result) => console.log('connected to db'))
         .catch((err) => console.log(err));
+
+app.listen(port, function(err){
+    console.log("Listening on port: " + port)
+});
